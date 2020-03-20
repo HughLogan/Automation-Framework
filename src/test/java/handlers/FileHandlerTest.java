a97395a8f06779aba696fba1b4ec91ab3d85e504
Manoj Kumar Olla <rveeranki@wsgc.com>
2020-03-20 05:58:15 -0700

    Added Excel Operations as Handlers

--- src/test/java/handlers/FileHandlerTest.java
+++ src/test/java/handlers/FileHandlerTest.java
@@ -2,35 +2,29 @@ package handlers;
 
 import org.testng.annotations.Test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
-import static org.testng.Assert.*;
-
 public class FileHandlerTest {
-    String pat = System.getProperty("user.dir");
+
+    String folderPath = "/src/test/resources/";
+    String absPath = System.getProperty("user.dir") + folderPath;
 
     @Test
     public void testReadFile() throws IOException {
-
-       String str = FileHandler.readFile(pat+"/src/main/resources/OpFil.txt");
-       System.out.println("Text in File"+str);
-
+        String str = FileHandler.readFile(absPath + "OpFil.txt");
+        System.out.println("Text in File" + str);
     }
 
     @Test
     public void testWriteToNewFile() throws IOException {
-        String str = FileHandler.readFile(pat+"/src/main/resources/OpFile.txt");
-        FileHandler.writeToFile(pat+"/src/main/resources/OpFile1.txt",str);
-
+        String str = FileHandler.readFile(absPath + "OpFile.txt");
+        FileHandler.writeToFile(absPath + "OpFile1.txt", str);
     }
+
     @Test
     public void testWriteToFile() throws IOException {
-        String str = FileHandler.readFile(pat+"/src/main/resources/OpFile.txt");
-        FileHandler.writeToFile(pat+"/src/main/resources/OpFile1.txt",str,true);
-
+        String str = FileHandler.readFile(absPath + "OpFile.txt");
+        FileHandler.writeToFile(absPath + "OpFile1.txt", str, true);
     }
 
 }
