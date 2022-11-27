1c1a0edaa61d156ddbf4f1b329e7d32633aa4344
Maverick <olla.manojkumar@gmail.com>
2022-11-27 11:07:02 +0530

    changes main class to testng test

--- src/test/java/TestInherit/Innherittest.java
+++ src/test/java/TestInherit/Innherittest.java
@@ -1,12 +1,13 @@
 package TestInherit;
 
-
 import inheritance.A;
 import inheritance.B;
+import org.testng.annotations.Test;
 
 public class Innherittest {
 
-    public static void main(String args[]){
+    @Test
+    public void testinh(){
         B a = new B();
         a.thro();
         a.katch();
