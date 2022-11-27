14cd49b6ebc9a4f5a8c3488fa96eeabdcccc73a7
Maverick <olla.manojkumar@gmail.com>
2022-11-27 10:54:46 +0530

    added inheritance example

---
+++ src/main/java/inheritance/B.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class B extends A{
+
+    public void thro(){
+        System.out.println("B throws");
+    }
+
+    public void katch(){
+        System.out.println("B catches");
+
+    }
+}
---
+++ src/test/java/TestInherit/Innherittest.java
@@ -0,0 +1,17 @@
+package TestInherit;
+
+
+import inheritance.A;
+import inheritance.B;
+
+public class Innherittest {
+
+    public static void main(String args[]){
+        B a = new B();
+        a.thro();
+        a.katch();
+        a.run();
+    }
+
+
+}
